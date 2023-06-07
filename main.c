e4d5e50ee2111be1f5fc9d3ac0f29907e5d76ba1
vladislavpocheptsov <Pocheptsov.vladi@gmail.com>
2023-06-07 18:22:39 +0400

    комит #2

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-main()
-{
+
+int main() {
+    int a, b, sum;
+    printf("¬ведите два целых числа: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("—умма чисел %d и %d равна %dn", a, b, sum);
+    return 0;
 }
